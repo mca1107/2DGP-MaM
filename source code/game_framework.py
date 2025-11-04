3ac5e1c52c9be52af60cdc2104834b1e925bfc44
mca1107 <moonchoa7@gmail.com>
2025-11-04 19:45:55 +0900

    game_framework.py 파일 수정

--- source code/game_framework.py
+++ source code/game_framework.py
@@ -1,3 +1,5 @@
+import time
+frame_time = 0.0
 running = None
 stack = None
 def change_mode(mode):
@@ -28,10 +30,16 @@ def run(start_mode):
     running = True
     stack = [start_mode]
     start_mode.init()
+    global frame_time
+    frame_time = 0.0
+    current_time = time.time()
     while running:
         stack[-1].handle_events()
         stack[-1].update()
         stack[-1].draw()
+        frame_time = time.time() - current_time
+        frame_rate = 1.0 / frame_time
+        current_time += frame_time
     while (len(stack) > 0):
         stack[-1].finish()
         stack.pop()
