63c57dd4f6579d789abb90f7275da013339b7c9f
mca1107 <moonchoa7@gmail.com>
2025-11-04 19:44:41 +0900

    game_framework.py 파일 추가

---
+++ source code/game_framework.py
@@ -0,0 +1,37 @@
+running = None
+stack = None
+def change_mode(mode):
+    global stack
+    if (len(stack) > 0):
+        stack[-1].finish()
+        stack.pop()
+    stack.append(mode)
+    mode.init()
+def push_mode(mode):
+    global stack
+    if (len(stack) > 0):
+        stack[-1].pause()
+    stack.append(mode)
+    mode.init()
+def pop_mode():
+    global stack
+    if (len(stack) > 0):
+        stack[-1].finish()
+        stack.pop()
+    if (len(stack) > 0):
+        stack[-1].resume()
+def quit():
+    global running
+    running = False
+def run(start_mode):
+    global running, stack
+    running = True
+    stack = [start_mode]
+    start_mode.init()
+    while running:
+        stack[-1].handle_events()
+        stack[-1].update()
+        stack[-1].draw()
+    while (len(stack) > 0):
+        stack[-1].finish()
+        stack.pop()
