de9565acd97e961abc69949186a417993f88bd6d
mca1107 <moonchoa7@gmail.com>
2025-11-12 21:37:19 +0900

    player.py 파일 수정

--- source code/player.py
+++ source code/player.py
@@ -1,8 +1,7 @@
 from pico2d import *
 from sdl2 import *
 from state_machine import StateMachine
-import game_framework
-import game_world
+import game_framework, game_world
 
 time_out = lambda e: e[0] == 'TIMEOUT'
 def space_down(e):
@@ -30,7 +29,7 @@ def pause_up(e):
 
 # player move speed
 PIXEL_PER_METER = (10.0 / 2.0) # 10 pixel == 2 meter
-RUN_SPEED_KMPH = 8.0 # Km / Hour
+RUN_SPEED_KMPH = 20.0 # Km / Hour
 RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
 RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
 RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
@@ -38,8 +37,42 @@ RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
 TIME_PER_ACTION = 0.5 # 한 걸음 걷는데 걸리는 시간 0.5초
 ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
 FRAMES_PER_ACTION = 6 # 한 걸음 걷는데 필요한 프레임 6장
-animation_names = ['Idle', 'Run']
+animation_names = ['Run'] # Idle 애니메이션 추가
 resource_address = 'C:\\Users\\moonc\\OneDrive\\문서\\GitHub\\2DGP-MaM\\resource\\player\\'
+class Player:
+    images = None
+    def load_image(self):
+        if Player.images == None:
+            Player.images = {}
+            for name in animation_names:
+                Player.images[name] = [load_image(resource_address + name + " (%d)" % i + ".png") for i in range(1, 7)]
+    def __init__(self):
+        self.x, self.y = 768, 480
+        self.frame = 0
+        self.face_dir = 1
+        self.dir = 0
+        self.load_image()
+        self.STOP = Stop(self)
+        self.IDLE = Idle(self)
+        self.RUN = Run(self)
+        self.DASH = Dash(self)
+        self.JUMP = Jump(self)
+        self.state_machine = StateMachine(
+            self.IDLE,
+            { self.STOP : {interaction_up: self.IDLE, pause_up: self.IDLE},
+              self.IDLE : {right_down: self.RUN, left_down: self.RUN, space_down: self.JUMP, interaction_down: self.STOP, pause_down: self.STOP},
+              self.RUN : {right_up: self.IDLE, left_up: self.IDLE, shift_down: self.DASH, space_down: self.JUMP},
+              self.DASH : {time_out: self.RUN, }}
+        )
+    def update(self):
+        self.state_machine.update()
+    def handle_event(self, event):
+        self.state_machine.handle_state_event(('INPUT', event))
+    def draw(self):
+        self.state_machine.draw()
+        draw_rectangle(*self.get_bb())
+    def get_bb(self):
+        return self.x - 30, self.y - 50, self.x + 30, self.y + 10
 class Stop:
     def __init__(self, player):
         pass
@@ -59,20 +92,27 @@ class Idle:
     def exit(self, e):
         pass
     def do(self):
-        self.player.frame = (self.player.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % FRAMES_PER_ACTION
+        pass
     def draw(self):
         pass
 class Run:
     def __init__(self, player):
-        pass
+        self.player = player
     def enter(self, e):
-        pass
+        if right_down(e):
+            self.player.dir = self.player.face_dir = 1
+        elif left_down(e):
+            self.player.dir = self.player.face_dir = -1
     def exit(self, e):
         pass
     def do(self):
-        pass
+        self.player.frame = (self.player.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % FRAMES_PER_ACTION
+        self.player.x += self.player.dir * RUN_SPEED_PPS * game_framework.frame_time
     def draw(self):
-        pass
+        if self.player.face_dir == 1:
+            Player.images['Run'][int(self.player.frame)].draw(self.player.x, self.player.y, 100, 100)
+        elif self.player.face_dir == -1:
+            Player.images['Run'][int(self.player.frame)].composite_draw(0, 'h', self.player.x, self.player.y, 100, 100)
 class Dash:
     def __init__(self, player):
         pass
@@ -95,36 +135,3 @@ class Jump:
         pass
     def draw(self):
         pass
-class Player:
-    image = None
-    def load_image(self):
-        if Player.images == None:
-            Player.images = {}
-            for name in animation_names:
-                Player.images[name] = [load_image(resource_address + name + " (%d)" % i + ".png") for i in range(1, 7)]
-    def __init__(self):
-        self.x, self.y = 960, 540
-        self.frame = 0
-        self.face_dir = 1
-        self.dir = 0
-        self.load_image()
-        self.STOP = Stop(self)
-        self.IDLE = Idle(self)
-        self.RUN = Run(self)
-        self.DASH = Dash(self)
-        self.JUMP = Jump(self)
-        self.state_machine = StateMachine(
-            self.IDLE,
-            { self.STOP : {interaction_up: self.IDLE, pause_up: self.IDLE},
-              self.IDLE : {right_down: self.RUN, left_down: self.RUN, space_down: self.JUMP, interaction_down: self.STOP, pause_down: self.STOP},
-              self.RUN : {right_up: self.IDLE, left_up: self.IDLE, shift_down: self.DASH, space_down: self.JUMP},
-              self.DASH : {time_out: self.RUN, }}
-        )
-    def get_bb(self):
-        return self.x - 100, self.y - 100, self.x + 100, self.y + 100
-    def update(self):
-        self.state_machine.update()
-    def handle_event(self, event):
-        self.state_machine.handle_state_event(('INPUT', event))
-    def draw(self):
-        self.state_machine.draw()
