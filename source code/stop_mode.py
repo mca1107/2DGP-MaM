5cfc10e3ec656682d95f6ba76d58a65a54340e82
mca1107 <moonchoa7@gmail.com>
2025-11-12 20:10:12 +0900

    stop_mode.py 파일 수정

---
+++ source code/stop_mode.py
@@ -0,0 +1,27 @@
+from pico2d import *
+import play_mode_stage1
+import game_framework
+
+image = None
+running = True
+def handle_events():
+    event_list = get_events()
+    for event in event_list:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.pop_mode() # 원래 스테이지로 돌아가야 함
+def init():
+    global image, running
+    # image = load_image(?)
+def finish():
+    global image
+    del image
+def update():
+    pass
+def draw():
+    clear_canvas()
+    image.draw(960, 540)
+    update_canvas()
+def pause(): pass
+def resume(): pass
