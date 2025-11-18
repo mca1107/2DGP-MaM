fcfbaab1f481b7ca1675656954b2bf5dc4c51cf3
mca1107 <moonchoa7@gmail.com>
2025-11-19 06:15:20 +0900

    background.py 파일 수정, staging.py 파일 수정

--- source code/staging.py
+++ source code/staging.py
@@ -7,11 +7,12 @@ class Staging:
         self.y = y
         self.width = width
         self.height = height
-        if GD.stage_num == 1:
+        if GD.stage_num == 0:
             self.image = load_image(resource_address + 'stage1\\' + 'staging.png')
     def update(self):
         pass
     def draw(self):
+        self.image.draw(self.x, self.y)
         draw_rectangle(*self.get_bb())
     def get_bb(self):
         return self.x - self.width // 2, self.y - self.height // 2, self.x + self.width // 2, self.y + self.height // 2
