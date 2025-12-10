d9ede61920825a226f9c0e8ee9d2f3006c934665
mca1107 <moonchoa7@gmail.com>
2025-12-10 21:29:32 +0900

    main.py 파일 수정

--- source code/main.py
+++ source code/main.py
@@ -2,6 +2,6 @@ from pico2d import *
 import game_framework
 import global_data as GD
 import title_mode as start_mode
-open_canvas(1536, 960)
+open_canvas(GD.game_width, GD.game_height)
 game_framework.run(start_mode)
 close_canvas()
